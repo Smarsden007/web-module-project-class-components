f672a1cdab069dffaee3b07baf9b7434cd1bcadb
Smarsden007 <seannmarsden@gmail.com>
2022-04-11 22:31:43 -0700

    Creating base for app.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        {/* <h1>Todo App</h1> */}
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
